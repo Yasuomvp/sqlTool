8fea25bd8e5e4e627308cede227f1cedf5f34ddf
Yasuomvp <teemovv@outlook.com>
2022-10-26 11:52:48 +0800

    .

---
+++ src/main/java/com/rick/sqltool/SqlToolApplication.java
@@ -0,0 +1,13 @@
+package com.rick.sqltool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SqlToolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SqlToolApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/rick/sqltool/SqlToolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rick.sqltool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SqlToolApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
